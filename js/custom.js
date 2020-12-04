eb2d2260f0c2cf6b85a9f59120b2f69f9c314467
mcvazin <mcvazin@gmail.com>
2020-12-04 12:12:31 +0200

    ver 15

--- js/custom.js
+++ js/custom.js
@@ -33,4 +33,52 @@ $(document).ready(function () {
   $(".user-manager").on("click", function () {
     $(".manager-contacts").toggleClass("show-contact");
   });
+
+  /*select-center*/
+  //  Implementation
+var getTextWidth = function(text,textClass) {
+  var span = document.createElement('span') 
+  span.style.opacity = 0
+  span.style.display = 'inline-block'
+  span.innerHTML = text
+  if(textClass) span.classList.add(textClass)
+  var body = document.querySelector('body').appendChild(span)
+  var width = span.offsetWidth
+  span.remove()
+  return width
+}
+
+var getSelectOptionIndentToCenter = function (selectElement,textClass) {
+  var selectChoice = selectElement.querySelector('option:checked')
+  var emptySpace = selectElement.offsetWidth - getTextWidth(selectChoice.innerHTML,textClass)
+  return emptySpace / 2
+}
+
+var mutateSelectCenterOption = function (selectElement,textClass,offset) {
+  var centerOffset = getSelectOptionIndentToCenter(selectElement,textClass)
+  selectElement.style.textIndent = (centerOffset - offset) + 'px'
+}
+
+//  Wrapper
+
+var $$ = $$ ? $$ : function(selector,scope) {
+  var scope = scope ? scope : document
+  return Array.prototype.slice.call(scope.querySelectorAll(selector))
+}
+
+var mutateMultiSelectCenterOption = function(selector,textClass,offset) {
+  $$(selector).map(function(selectElement){
+    selectElement.addEventListener('change',function(event){
+      mutateSelectCenterOption(event.target,offset)
+    })
+    mutateSelectCenterOption(selectElement,textClass,offset)
+  }) 
+}
+
+// Example 
+
+mutateMultiSelectCenterOption('select','minimal',10)
+
+
+  /*select-center*/
 }); /*END JQuery*/
